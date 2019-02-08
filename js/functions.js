dd6ac64e9301d0271c0ebb80604e9a945622e60d
Alex <pumpcin14@mail.ru>
2019-02-08 19:38:05 +0300

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -2,21 +2,18 @@
 window.timeStr = function () {
 let date = new Date();
 let h = date.getHours(), i = date.getMinutes(), s = date.getSeconds();
-return (h < 10 ? h : '0' + h ) + ":" + (i < 10 ? i : '0' + i ) + ":" + (s < 10 ? s : '0' + s );
+return (h >= 9 ? h : '0' + h ) + ":" + (i >= 9 ? i : '0' + i ) + ":" + (s >= 9 ? s : '0' + s );
 };
 window.timeToStr = function (time) {
 let str = '';
 let h = Math.floor(time / 60 / 60),
 i = Math.floor(time / 60 - h * 60),
 s = time % 60;
-if( h < 10 )
-h = '0' + h;
+if( h < 10 ){h = '0' + h};
 str += h + ':';
-if( i < 10 )
-i = '0' + i;
+if( i < 10 ){i = '0' + i};
 str += i + ':';
-if( s < 10 )
-s = '0' + s;
+if( s < 10 ){s = '0' + s};
 str += s;
 return str;
 };
