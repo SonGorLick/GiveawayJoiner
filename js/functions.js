d4182eae106ab76524f8995e3a044b741f16a88e
Alex <pumpcin14@mail.ru>
2019-02-08 18:26:25 +0300

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -7,11 +7,13 @@ return (h < 10 ? h : '0' + h ) + ":" + (i < 10 ? i : '0' + i ) + ":" + (s < 10 ?
 window.timeToStr = function (time) {
 let str = '';
 let h = Math.floor(time / 60 / 60),
-i = Math.floor(( time - (h*60) ) / 60),
+i = Math.floor(time / 60 - h * 60),
 s = time % 60;
-if( h > 0 )
+if( h < 10 )
+h = '0' + h;
 str += h + ':';
-if( i > 0 )
+if( i < 10 )
+i = '0' + i;
 str += i + ':';
 if( s < 10 )
 s = '0' + s;
