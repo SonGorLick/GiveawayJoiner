8b286fdba8e9d72337d29c90ab2e7f12a9c85281
Alex <pumpcin14@mail.ru>
2019-02-24 21:01:24 +0300

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -6,15 +6,10 @@ return (h > 9 ? h : '0' + h ) + ":" + (i > 9 ? i : '0' + i ) + ":" + (s > 9 ? s 
 };
 window.timeToStr = function (time) {
 let str = '';
-let h = Math.floor(time / 60 / 60),
+let h = Math.floor(time / 3600),
 i = Math.floor(time / 60 - h * 60),
 s = time % 60;
-if( h < 10 ){h = '0' + h}
-str += h + ':';
-if( i < 10 ){i = '0' + i}
-str += i + ':';
-if( s < 10 ){s = '0' + s}
-str += s;
+str = (h > 9 ? h : '0' + h ) + ':' + (i > 9 ? i : '0' + i ) + ':' + (s > 9 ? s : '0' + s );
 return str;
 };
 window.async = function(func, callback) {
