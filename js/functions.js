29a06155272e32a41701eefb091d092cf97f6dc8
Alex <pumpcin14@mail.ru>
2019-03-21 02:51:42 +0300

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -5,8 +5,8 @@ let h = date.getHours(), i = date.getMinutes(), s = date.getSeconds();
 return (h > 9 ? h : '0' + h) + ':' + (i > 9 ? i : '0' + i) + ':' + (s > 9 ? s : '0' + s);
 };
 window.timeToStr = function (time) {
-let str = '';
-let h = Math.floor(time / 3600),
+let str = '',
+h = Math.floor(time / 3600),
 i = Math.floor(time / 60 - h * 60),
 s = time % 60;
 str = (h > 9 ? h : '0' + h) + ':' + (i > 9 ? i : '0' + i) + ':' + (s > 9 ? s : '0' + s );
