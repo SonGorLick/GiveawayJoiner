17d9aa2632c673918be13186f6643b0cee209b82
Alex <pumpcin14@mail.ru>
2019-11-21 20:48:31 +0300

    Add files via upload

--- js/services/astats.js
+++ js/services/astats.js
@@ -14,7 +14,7 @@ this.settings.blacklist_on = { type: 'checkbox', trans: this.transPath('blacklis
 super.init();
 }
 getUserInfo(callback) {
-if (GJuser.as.length > 601) {
+if (GJuser.as.length > 801) {
 GJuser.as = ',';
 }
 let userData = {
