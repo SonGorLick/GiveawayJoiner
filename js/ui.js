08a9e6fc4cdf1b523d82925b84adfa7ad27a1c68
Alex <pumpcin14@mail.ru>
2020-05-14 19:32:55 +0300

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -141,6 +141,7 @@ $(document.createElement('button'))
 }
 function renderUser(userData) {
 $('.content-item .info .username').html('GiveawayJoiner');
+if (!Config.get('steam_local', false)) {
 $.ajax({
 url: 'https://store.steampowered.com/account',
 success: function (data) {
@@ -157,6 +158,7 @@ $('.content-item .info .username').html(userData.username);
 }
 }
 });
+}
 }
 function openWebsite(url) {
 Browser.loadURL(url);
